cdc4fcf8c288bd3b537582cb7cd63951273830bd
Emediong Francis <emediongfrancis@gmail.com>
2022-08-05 23:20:31 +0100

    Clean repo

--- User/serializers.py
+++
@@ -1,8 +0,0 @@
-from pyexpat import model
-from rest_framework import serializers 
-from .models import Users
-
-class Userserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Users
-        fields = ['id', 'firstname', 'lastname','email', 'phone', 'address']
